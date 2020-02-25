e5ec7a74a01d940da3d79b5f8f8a2b0704b8844e
Jerome Forissier <jerome@forissier.org>
2020-02-25 17:45:33 +0100

    libutils: add simplified fputc(), fputs(), fwrite(), write()
    Preparing for C++ support in TAs.
    
    Adds a few <stdio.h> functions to libutils:
    
     fputc()  fputs()  fwrite()  write()
    
    The proposed implementations are limited in the sense that they only
    accept writing to stdout or stderr. The output goes directly to the
    secure console and no difference is made between stdout and stderr.
    
    Signed-off-by: Jerome Forissier <jerome@forissier.org>

---
+++ lib/libutils/isoc/fp.c
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2020, Huawei Technologies Co., Ltd
+ */
+
+#include <stdio.h>
+
+struct _FILE {
+};
+
+static struct _FILE _fake_stdout;
+static struct _FILE _fake_stderr;
+
+FILE *stdout = &_fake_stdout;
+FILE *stderr = &_fake_stderr;
